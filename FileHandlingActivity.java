b732344c28ef1e662304d7b8bb3209fcb23d273a
MikeHi1 <mikeneverd3@gmail.com>
2025-09-15 12:34:34 -0700

    FMC 1.0.2 Three New Files
    Added and tested a method that creates 3 empty files in the root folder that contains very valuable information!

--- FileHandlingActivity.java
+++ FileHandlingActivity.java
@@ -1,12 +1,18 @@
 import java.io.*;
 
 public class FileHandlingActivity {
-    public static void main(String[] args) {        
-        // a. Create main directory
+    public static void main(String[] args) throws IOException {        
+
         File dir = new File("Maine");
         dir.mkdir();
 
         // b. Create three text files
+        File creditCard = new File("myCreditCard");
+        creditCard.createNewFile();
+        File SSN = new File("mySSN");
+        SSN.createNewFile();
+        File passwords = new File("myPasswords");
+        passwords.createNewFile();
         
         // c. Write messages to files
         
