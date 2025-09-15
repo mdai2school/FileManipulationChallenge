b90fc8c78a86d69be881723829fb02f486b0d20f
MikeHi1 <mikeneverd3@gmail.com>
2025-09-15 12:39:51 -0700

    FMC 1.0.3 Filewriter
    Added 3 Filewriters (making sure to close them after) that filled in information for the 3 files created previously! Also tested and works!

--- FileHandlingActivity.java
+++ FileHandlingActivity.java
@@ -6,16 +6,23 @@ public class FileHandlingActivity {
         File dir = new File("Maine");
         dir.mkdir();
 
-        // b. Create three text files
         File creditCard = new File("myCreditCard");
         creditCard.createNewFile();
         File SSN = new File("mySSN");
         SSN.createNewFile();
         File passwords = new File("myPasswords");
         passwords.createNewFile();
-        
+
         // c. Write messages to files
-        
+        FileWriter pencil = new FileWriter(creditCard);
+        pencil.write("My credit card number is 15!");
+        pencil.close();
+        FileWriter pen = new FileWriter(SSN);
+        pen.write("My SSN is 21!");
+        pen.close();
+        FileWriter sharpie = new FileWriter(passwords);
+        sharpie.write("My password is 1234!");
+        sharpie.close();
         // d. Read and display file contents
         
         // e. Create backup directory
